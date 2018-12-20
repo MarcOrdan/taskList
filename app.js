b10e26390390d42d2b5f4e58bef5ed2fae1bf387
Mac Ordan <marcordan@gmail.com>
2018-12-20 12:22:19 +0800

    added code for filter tasks

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
     //CLEAR TASKS EVENT
     clearBtn.addEventListener('click', clearTasks)
+    //FILTER TASKS EVENT
+    filter.addEventListener('keyup', filterTasks)
 
 }
 
@@ -69,4 +71,19 @@ function clearTasks(){
    while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
    }
+}
+
+//FILTER TASKS
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+    (function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text)!= -1){
+            task.style.display ='block';
+        } else {
+            task.style.display ='none';
+        }
+    });
 }
